c70869624041d711f9bbe255fece2a3cc624c7c6
travishjames <travishjames@gmail.com>
2016-08-31 14:44:15 -0700

    Update 05- Python Advanced- Code

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,4 +1,3 @@
-Q1. 
 import csv
 import urllib2
 import re
@@ -7,6 +6,8 @@ url = "https://raw.githubusercontent.com/travishjames/dsp/master/python/faculty.
 response = urllib2.urlopen(url)
 cr = csv.reader(response)
 list_all = list(cr)
+
+Q1
 
 total_degrees = [item[1] for item in list_all]
 unique_degrees = {}
@@ -24,15 +25,6 @@ print(len(unique_degrees))
 
 Q2.
 
-import csv
-import urllib2
-import re
-
-url = "https://raw.githubusercontent.com/travishjames/dsp/master/python/faculty.csv"
-response = urllib2.urlopen(url)
-cr = csv.reader(response)
-list_all = list(cr)
-
 total_titles = [item[2] for item in list_all]
 unique_titles = {}
 for t in range(1, len(total_titles)):
@@ -47,6 +39,17 @@ print(len(unique_titles))
 
 Q3.
 
+emails = [list_all[item][3] for item in range(1, len(list_all))]
+
+print(emails)
 
 Q4.
 
+emails = [list_all[item][3] for item in range(1, len(list_all))]
+domains = set()
+for item in emails:
+	email = item.split("@")[-1]
+	domains.add(email)
+	
+print(domains)
+print(len(domains))

31122d2dd82ece4a9d66dd4223a8dc902519f27c
travishjames <travishjames@gmail.com>
2016-08-31 14:21:02 -0700

    Update 05- Python Advanced- Code

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -24,6 +24,26 @@ print(len(unique_degrees))
 
 Q2.
 
+import csv
+import urllib2
+import re
+
+url = "https://raw.githubusercontent.com/travishjames/dsp/master/python/faculty.csv"
+response = urllib2.urlopen(url)
+cr = csv.reader(response)
+list_all = list(cr)
+
+total_titles = [item[2] for item in list_all]
+unique_titles = {}
+for t in range(1, len(total_titles)):
+	title = total_titles[t].rsplit(' ', 2)[0] #cutting off final two words from title since redundant and irrelevant
+	if title in unique_titles:
+		unique_titles[title] += 1
+	else:
+		unique_titles[title] = 1
+
+print(unique_titles)
+print(len(unique_titles))
 
 Q3.
 
